5e00c82503345a730cd11e3c37e10fc8fc4971e8
Jorge Eduardo <jandcompany22@gmail.com>
2025-05-30 16:19:23 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -248,7 +248,7 @@ function App() {
                 } />
 
                 {/* Catch all - redirect to dashboard */}
-                  <Route path="*" element={<Navigate to="/" replace />} />
+                  <Route path="*" element={<Navigate to="/\" replace />} />
               </Route>
             </Routes>
             </Suspense>

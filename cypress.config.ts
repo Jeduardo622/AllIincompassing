ec3acb0d9e360ca7438cb4f95d6c8d21677efed6
Jorge Eduardo <jandcompany22@gmail.com>
2025-09-23 18:24:55 -0700

    chore(cypress): upgrade to v13

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from 'cypress';
+import viteConfig from './vite.config';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config;
     },
   },
   component: {
+    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/component.ts',
     devServer: {
       framework: 'react',
       bundler: 'vite',
+      viteConfig,
     },
   },
 });

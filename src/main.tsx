79e465120afa5dd899b5b620dbfb6a60bfd57e82
Jorge Eduardo <jandcompany22@gmail.com>
2025-10-04 06:05:44 -0700

    fix(bootstrap): correct app import destructuring

--- src/main.tsx
+++ src/main.tsx
@@ -53,7 +53,7 @@ const RuntimeConfigError: React.FC<{ message: string }> = ({ message }) => (
 
 const bootstrap = async (): Promise<void> => {
   try {
-    const [{ default: App }] = await Promise.all([ensureRuntimeSupabaseConfig(), import('./App.tsx')]);
+    const [, { default: App }] = await Promise.all([ensureRuntimeSupabaseConfig(), import('./App.tsx')]);
     root.render(
       <React.StrictMode>
         <DevErrorBoundary>

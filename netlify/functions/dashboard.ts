2da9bb153fb974c9811286e9691deca5a7fae4dc
Jeduardo622 <test@example.com>
2025-12-22 09:51:19 -0800

    change to netlify function

---
+++ netlify/functions/dashboard.ts
@@ -0,0 +1,45 @@
+import { Handler } from '@netlify/functions';
+import { dashboardHandler } from '../../src/server/api/dashboard';
+
+const toNetlifyResponse = async (response: Response) => {
+  const headers: Record<string, string> = {};
+  response.headers.forEach((value, key) => {
+    headers[key] = value;
+  });
+
+  return {
+    statusCode: response.status,
+    headers,
+    body: await response.text(),
+    isBase64Encoded: false,
+  };
+};
+
+export const handler: Handler = async (event) => {
+  try {
+    const bodyNeeded = event.httpMethod !== 'GET' && event.httpMethod !== 'HEAD';
+    const body =
+      bodyNeeded && event.body
+        ? event.isBase64Encoded
+          ? Buffer.from(event.body, 'base64')
+          : event.body
+        : undefined;
+
+    const request = new Request(event.rawUrl || `https://${event.headers.host}${event.path}`, {
+      method: event.httpMethod,
+      headers: event.headers as HeadersInit,
+      body,
+    });
+
+    const response = await dashboardHandler(request);
+    return toNetlifyResponse(response);
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
+
+
